6d6a533b2529cd05677a589d9b412eb608e48371
Denis Chornonog <onlinesvxonline@mail.ru>
2024-05-14 13:31:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+using System.Xml;
+
+class Program
+{
+    static void Main()
+    {
+        string jsonString = "{\"Name\": \"Денис\", \"LastName\": \"Чорноног\", \"Age\": 35, \"City\": \"Москва\"}";
+
+        using (JsonDocument jsonDoc = JsonDocument.Parse(jsonString))
+        {
+            using (XmlWriter xmlWriter = XmlWriter.Create(Console.Out))
+            {
+                xmlWriter.WriteStartElement("Root");
+
+                foreach (JsonProperty property in jsonDoc.RootElement.EnumerateObject())
+                {
+                    xmlWriter.WriteStartElement(property.Name);
+                    xmlWriter.WriteString(property.Value.ToString());
+                    xmlWriter.WriteEndElement();
+                }
+
+                xmlWriter.WriteEndElement();
+            }
+        }
+    }
+}
